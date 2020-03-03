1e332ee19c04ac006b198b82a21fa60a4eacfa55
StarCrystal Team <44244999+StarCrystalTeam@users.noreply.github.com>
2020-03-03 16:59:20 -0500

    Add warning

--- hacksinject.js
+++ hacksinject.js
@@ -1,3 +1,4 @@
+//DO NOT USE THE GAME HACKS ON YOUR PRIMARY ACCOUNT! MAKE AN ALT PRODIGY ACCOUNT!
 PIXI.game.prodigy.player.data.level = 100;
 PIXI.game.prodigy.player.isBlockedByDarkTowerMemberGate = false;
 PIXI.game.prodigy.player.data.hp = 9000;
